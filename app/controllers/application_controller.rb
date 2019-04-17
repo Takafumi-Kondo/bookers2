55e9c2e210dc2432cdc08068f21f1dd2166ce63e
kondo <aa@kondo.local>
2019-04-16 20:34:10 -0700

    [Add] 5-9 ユーザ登録ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	#devise利用する前にconfigure_permitted_parametersが実行
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected#他コントローラからも
+#サインアップ時ユーザnameのデータ操作許可
+	def configure_permitted_parameters
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	end
 end
